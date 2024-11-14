bfd908e4c6c5faaceefd7c57f164993785a1bb5a
andreasevansdurhamcollege <andreas.evansadamecz@dcmail.ca>
2024-11-13 23:27:03 -0500

    finished a few minor methods and began working on GameButton_Click()

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -16,9 +16,56 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string XPlayer = "X";
+        private const string OPlayer = "O";
+        private string currentPlayer;
+        private Button[,] buttons;
+        private int XPlayerScore = 0;
+        private int OPlayerScore = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeGame();
         }
+
+        private void InitializeGame()
+        {
+            buttons = new Button[3, 3]
+            {
+                {btn00, btn01, btn02},
+                {btn10, btn11, btn12},
+                {btn20, btn21, btn22},
+            };
+
+            foreach (Button button in buttons) 
+            {
+                button.Content = string.Empty;
+                button.IsEnabled = true;
+                button.Background = Brushes.White;
+    
+            }
+
+            currentPlayer = XPlayer;
+            UpdateCurrentPlayerTxtBlock();
+        }
+
+        private void UpdateCurrentPlayerTxtBlock() 
+        {
+            txtblock_CurrentPlayer.Text = $"Current player: {currentPlayer}";
+        }
+
+        private void SwitchTurns() 
+        {
+            currentPlayer = currentPlayer == XPlayer ? OPlayer : XPlayer;
+            UpdateCurrentPlayerTxtBlock();
+        }
+
+        private void GameButton_Click(object sender, RoutedEventArgs e) 
+        {
+
+        }
+
+        
     }
 }
